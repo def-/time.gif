53546b8d53c15250b9a2cc6988b29e2067ee1744
def <dennis@felsin9.de>
2017-08-12 10:49:08 +0200

    Clean up a bit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 time.gif
 ========
 
+Compile with -threaded, otherwise GHC networking has problems with more than 1024 open connections.
+
 [https://hookrace.net/blog/time.gif](https://hookrace.net/blog/time.gif)
--- Time.hs
+++ Time.hs
@@ -1,89 +1,11 @@
-import System.Random
 import GifStream
+import Numbers
+
+import System.Random
 import Data.Time.Clock
 import Data.Time.LocalTime
 import Data.List
 
--- Stopping focus of the browser tab stops the animation. Reload the page to fix it.
-
-type Position = (Int,Int)
-
-numbers =
-  [ [ [1,1,1]
-    , [1,0,1]
-    , [1,0,1]
-    , [1,0,1]
-    , [1,1,1] ]
-
-  , [ [0,1,0]
-    , [0,1,0]
-    , [0,1,0]
-    , [0,1,0]
-    , [0,1,0] ]
-
-  , [ [1,1,1]
-    , [0,0,1]
-    , [1,1,1]
-    , [1,0,0]
-    , [1,1,1] ]
-
-  , [ [1,1,1]
-    , [0,0,1]
-    , [0,1,1]
-    , [0,0,1]
-    , [1,1,1] ]
-
-  , [ [1,0,1]
-    , [1,0,1]
-    , [1,1,1]
-    , [0,0,1]
-    , [0,0,1] ]
-
-  , [ [1,1,1]
-    , [1,0,0]
-    , [1,1,1]
-    , [0,0,1]
-    , [1,1,1] ]
-
-  , [ [1,1,1]
-    , [1,0,0]
-    , [1,1,1]
-    , [1,0,1]
-    , [1,1,1] ]
-
-  , [ [1,1,1]
-    , [0,0,1]
-    , [0,0,1]
-    , [0,0,1]
-    , [0,0,1] ]
-
-  , [ [1,1,1]
-    , [1,0,1]
-    , [1,1,1]
-    , [1,0,1]
-    , [1,1,1] ]
-
-  , [ [1,1,1]
-    , [1,0,1]
-    , [1,1,1]
-    , [0,0,1]
-    , [1,1,1] ]
-
-  , [ [0]
-    , [0]
-    , [0]
-    , [0]
-    , [0] ]
-
-  , [ [0,0,0]
-    , [0,1,0]
-    , [0,0,0]
-    , [0,1,0]
-    , [0,0,0] ]
-  ]
-
--- 30000 seems to be the lowest value that works in Firefox
--- 30 ms => 33 fps
 delay = 1000000 -- in µs
 port = 5002
 
@@ -100,19 +22,21 @@ logic wait getInput sendFrame = go
     go = do
       now <- getCurrentTime
       let (TimeOfDay h m s) = localTimeOfDay $ utcToLocalTime utc now
-      let nums = append $ map (numbers !!) $ intersperse 10 [h `div` 10, h `mod` 10, 11, m `div` 10, m `mod` 10, 11, (floor s) `div` 10, (floor s) `mod` 10]
-      let framedNums = map (map colorize) (frame nums)
-      sendFrame (scale zoom framedNums)
 
+      let display = map (map colorize) $ frame $
+            foldr1 (zipWith (++)) $ map (numbers !!) $ intersperse 10
+              [ h `div` 10, h `mod` 10, 11
+              , m `div` 10, m `mod` 10, 11
+              , (floor s) `div` 10, (floor s) `mod` 10
+              ]
+
+      sendFrame (scale zoom display)
       wait
       go
 
-append [x] = x
-append (x:xs) = zipWith (++) x (append xs)
+frame xs = map (preappend 0) $ preappend (take (length (xs !! 0)) (repeat 0)) xs
 
-frame xs = map framepart (z : xs ++ [z])
-  where z = take (length (xs !! 0)) (repeat 0)
-        framepart xs = 0 : xs ++ [0]
+preappend x xs = x : xs ++ [x]
 
 colorize 1 = (3,3,3)
 colorize 0 = (0,0,0)
